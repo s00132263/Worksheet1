bf3768d9713ca22cff57e635add491863831a9fa
Raimonds Raginskis <S00132263@mail.itsligo.ie>
2019-02-02 13:12:46 +0000

    a

--- Worksheet1/pages/WS1b.cshtml.cs
+++ Worksheet1/pages/WS1b.cshtml.cs
@@ -12,6 +12,7 @@ namespace Worksheet1.pages
 
         public string Message { get; set; }
 
+
         [BindProperty]
         public int Number1 { get; set; }
 
@@ -23,9 +24,26 @@ namespace Worksheet1.pages
 
         }
 
-        public void OnPost()
+        public void OnPost(string firstname, int Number1, int Number2, string operation)
         {
             Message = $"{Number1} + {Number2} = {Number1 + Number2}";
+
+            switch (operation)
+            {
+                case "plus":
+                    Message = $"{Number1} plus {Number2} equals {Number1 + Number2}";
+                    break;
+                case "minus":
+                    Message = $"{Number1} minus {Number2} equals {Number1 - Number2}";
+                    break;
+                case "multiply":
+                    Message = $"{Number1} multipled by {Number2} equals {Number1 * Number2}";
+                    break;
+                case "divide":
+                    float Num2 = (float)Number2; // necessary because result might not be an integer
+                    Message = $"{Number1} divided by {Number2} equals {Number1 / Num2}";
+                    break;
+            }
         }
     }
 }
