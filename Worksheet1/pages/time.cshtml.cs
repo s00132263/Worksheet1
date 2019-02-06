e7521d58e694e274bb013519528a973f185d88ab
Raimonds Raginskis <S00132263@mail.itsligo.ie>
2019-02-06 20:03:27 +0000

    aa

---
+++ Worksheet1/pages/time.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Worksheet1.pages
+{
+    public class timeModel : PageModel
+    {
+        public string Message { get; set; } = "";
+
+        public string[] DaysArray { get; set; } = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+        [BindProperty]
+        public bool[] ChosenDay { get; set; } = new bool[7];
+
+
+        [BindProperty]
+        public bool[] MonthChosen { get; set; } = new bool[5];
+
+        public string[] MonthList { get; set; } = { "January", "February", "March", "April", "May" };
+
+        [BindProperty]
+        public string[] ChosenMonths { get; set; } = { };
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost( )
+        {
+           
+           
+        }
+    }
+}
